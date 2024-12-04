39d58c0f0464926b149e881efd150e775d4a2707
Huy Nguyen <kokoanimal@gmail.com>
2024-12-03 23:40:23 -0800

    Parsing for school information and description

--- query.py
+++ query.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import random
+import json
 
 # url = "https://httpbin.io/user-agent"
 url = "https://www.zillow.com/homedetails/5157-Chena-Hot-Springs-Rd-Fairbanks-AK-99712/74498488_zpid/"
@@ -32,6 +33,10 @@ response_contents = response.text
 
 soup = BeautifulSoup(response_contents, "html.parser")
 
+# parse a description
+description = soup.find_all("div", {"data-testid": "description"})
+print(description[0].text)
+
 # parse to find the listed price
 price = soup.find_all("span", {"data-testid": "price"})
 print(price[0].text)
@@ -42,4 +47,34 @@ info = soup.find_all("div", {"data-testid": "bed-bath-sqft-fact-container"})
 for i in range(info):
     print(info[i].text)
 
-# "GreatSchools rating"
+res = []
+json_string = soup.find_all("script", {"id": "__NEXT_DATA__"})[0].string
+test = json.loads(json_string)
+# load the json string
+json_convert = json.loads(
+    test["props"]["pageProps"]["componentProps"]["gdpClientCache"]
+)
+
+first_key = next(iter(json_convert))
+
+schools = json_convert[first_key]["property"]["schools"]
+
+for school in schools:
+    print(
+        " ".join(
+            [
+                school["name"],
+                f'({school["type"]}, {school["level"]})',
+                "rated",
+                str(school["rating"]),
+                "out of 10 at a distance of",
+                str(school["distance"]),
+                "miles.",
+            ]
+        )
+    )
+# with open("record2.json", "w", encoding="utf-8") as file:
+#     file.write(test)
+
+# with open("record.json", "w", encoding="utf-8") as f:
+#     json.dump(json_convert, f)
